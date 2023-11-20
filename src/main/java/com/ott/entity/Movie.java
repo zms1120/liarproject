13343fd53aa515e0083c2b7536021194c0372e3d
annie <atthe09624@gmail.com>
2023-11-20 13:38:36 +0900

    [update] movie 엔티티 수정

--- src/main/java/com/ott/entity/Movie.java
+++ src/main/java/com/ott/entity/Movie.java
@@ -1,6 +1,7 @@
 package com.ott.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,13 +17,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
+	@Id
 	private String movie_code; // 영화 일련번호(국내:k, 해외:f)
 	private String mTitle_ko; // 영화제목 국문
 	private String mTitle_ori; // 영화제목 원제
 	private String category; // 카테고리
 	private String genre; // 장르
 	private String nation; // 제작 국가
-	private String prodYear; // 제작 년도
+	private String openYear; // 개봉년도
 	private String director; // 감독
 	private String cast; // 출연진
 	private String rating; // 관람등급
