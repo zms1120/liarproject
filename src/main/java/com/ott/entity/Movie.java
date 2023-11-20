6d4330323207052ac0fb95b76b96409e28d7a8f3
annie <atthe09624@gmail.com>
2023-11-20 10:10:43 +0900

    [update] Movie 엔티티 최종본

--- src/main/java/com/ott/entity/Movie.java
+++ src/main/java/com/ott/entity/Movie.java
@@ -16,13 +16,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
-	private String mseq;
-	private String mNameko;
-	private String mNameEn;
-	private String prdtYear;
-	private String openDt;
-	private String genreAlt;
-	private String directors;
-	private String companys;
-	private String thumnail_path;
+	private String movie_code; // 영화 일련번호(국내:k, 해외:f)
+	private String mTitle_ko; // 영화제목 국문
+	private String mTitle_ori; // 영화제목 원제
+	private String category; // 카테고리
+	private String genre; // 장르
+	private String nation; // 제작 국가
+	private String prodYear; // 제작 년도
+	private String director; // 감독
+	private String cast; // 출연진
+	private String rating; // 관람등급
+	private String running_time; // 러닝타임
+	private String keyword; // 영화 내용 키워드
+	private String description; // 줄거리, 상세설명
+	private String image_path; // 썸네일 이미지 경로
 }
